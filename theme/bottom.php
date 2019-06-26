dde5caf8fb02efb5bd873dd554e4e7b11f722142
Txawey <masterofellusion@gmail.com>
2019-06-25 22:11:11 -0400

    Episode 6: Return of the Repairs

--- theme/bottom.php
+++ theme/bottom.php
@@ -4,7 +4,7 @@ echo '</div>
 <div class="foot"></div>
 <!--JavaScript at end of body for optimized loading-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="'. $root . '/js/materialize.js"></script>
+<script type="text/javascript" src="'. $root . 'js/materialize.js"></script>
 <script>
 $(document).ready(function(){
   $(".sidenav").sidenav();
